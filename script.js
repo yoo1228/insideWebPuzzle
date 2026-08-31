9399c41747a3dc9570ded0384594e089adc1aef1
yoo1228 <yokawasaki1228@gmail.com>
2026-08-31 15:15:19 +0900

    add Stage1,2,3

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputElement = document.getElementById('key');
+const buttonElement = document.getElementById('enter');
+
+buttonElement.addEventListener('click', () => {
+
+    const inputValue = inputElement.value;
+
+    window.location.href = `file:///C:/Users/yo/Documents/GitHub/insideWebPuzzle/${inputValue}.html`;
+});
